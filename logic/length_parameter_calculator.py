9453dbd0a6a762ade102026b50a4d12065a15174
amirAHS2000 <ahs.amirhosein78@gmail.com>
2022-06-05 21:50:49 +0430

    add a function for updating request table after calculating length parameter.

--- logic/length_parameter_calculator.py
+++ logic/length_parameter_calculator.py
@@ -1,11 +1,13 @@
 import math
 
-
 # we need tf * idf of each document to calculate length of document-vector
 # tf_idf_of_document : this is a list of tf * idf of each term (word) in document
-def length_calculator(tf_idf_of_document):
+from datasource.models import ReqWordModel
+
+
+def length_calculator(tf_idf_of_document: list[ReqWordModel]):
     square_value = 0.0
     for tf_idf in tf_idf_of_document:
-        square_value += tf_idf ** 2
+        square_value += tf_idf.tfidf ** 2
 
     return math.sqrt(square_value)
