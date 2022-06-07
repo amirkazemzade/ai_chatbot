b2b6bf5eb1c89ba2b53ee55147c976a3b15f464b
amirAHS2000 <ahs.amirhosein78@gmail.com>
2022-06-07 20:55:16 +0430

    cosine_similarity_calculator function completed. and create a function for finding response that is correspond to best matches document.

--- logic/query_processor.py
+++ logic/query_processor.py
@@ -7,12 +7,12 @@ from logic import term_frequency_calculator
 class QueryProcessor:
     repository = Repository()
 
-    def __init__(self, user_model, query):
+    def __init__(self, user_model):
         self.user = user_model
-        self.query_text = query
-        self.tf_list = [0.0] * (len(query.split(" ")))
-        self.idf_list = [1.0] * (len(query.split(" ")))
-        self.tf_by_idf_list = [0.0] * (len(query.split(" ")))
+        self.query_text = user_model.query
+        self.tf_list = [0.0] * (len(user_model.query.split(" ")))
+        self.idf_list = [1.0] * (len(user_model.query.split(" ")))
+        self.tf_by_idf_list = [0.0] * (len(user_model.query.split(" ")))
         self.length_value = 0.0
 
     def tf_calculator(self):
@@ -51,9 +51,36 @@ class QueryProcessor:
         self.length_value = math.sqrt(square_value)
 
     def cosine_similarity_calculator(self):
-        # todo : we need calculate the cosine_similarity_value for each document that we have
-        # for example cosine_similarity(d1 , query) , ....
+        documents_list = self.repository.fetch_all_requests()
 
+        best_document_model = None
+        # maximum cosine_similarity_value that we obtained
+        best_document_model_value = 0.0
 
+        split_request = self.query_text
+        # loop through the list of all documents
+        for document in documents_list:
+            current_cosine_similarity_value = 0.0
+            split_current_document = document.req.split(" ")
+            for i in range(len(split_request)):
+                # if a word exist in both of document and request then we use it (tf*idf value in cosine similarity
+                # value)
+                if split_request[i] in split_current_document:
+                    request_tf_by_idf_value = self.tf_by_idf_list[i]
+                    document_tf_by_idf_value = self.repository.fetch_req_word_by_req_id_word_id(
+                        self.repository.find_word_by_string(split_request[i]).id, document.id).tfidf
+                    current_cosine_similarity_value += request_tf_by_idf_value * document_tf_by_idf_value
 
-        pass
+            current_cosine_similarity_value /= (self.length_value * document.length)
+            if current_cosine_similarity_value > best_document_model_value:
+                best_document_model_value = current_cosine_similarity_value
+                best_document_model = document
+
+        return best_document_model
+
+    def find_response_corresponding_request(self):
+        document_model = self.cosine_similarity_calculator()
+        # todo
+        response_model = self.repository.fetch_response_by_request()
+
+        return response_model
