96a7cc44cdeedaa4ed6f971df30a4853934791fc
amirAHS2000 <ahs.amirhosein78@gmail.com>
2022-06-09 13:32:21 +0430

    add three function in query_processor for initiate a user when bot start.

--- logic/query_processor.py
+++ logic/query_processor.py
@@ -2,25 +2,36 @@ import math
 
 from datasource import Repository
 from logic import term_frequency_calculator
+from telebot import types
 
 
 class QueryProcessor:
     repository = Repository()
 
-    def __init__(self, user_model):
-        self.user = user_model
-        # self.query_text = user_model.query
+    def __init__(self):
         self.last_response = ""
         self.last_product = None
 
-        # # these parameters should calculate for each query
-        # self.tf_list = [0.0] * (len(user_model.query.split(" ")))
-        # self.idf_list = [1.0] * (len(user_model.query.split(" ")))
-        # self.tf_by_idf_list = [0.0] * (len(user_model.query.split(" ")))
-        # self.length_value = 0.0
+    def user_start(self, user_telegram_model: types.User):
+        user_id = self.create_user(user_telegram_model.id)
+        user_shopping_list_id = self.create_user_shopping_list(user_id)
+        return "{} سلام".format(user_telegram_model.first_name)
 
-    def define_parameter(self):
-        pass
+    def create_user(self, user_telegram_id):
+        user_model = self.repository.fetch_user_by_tel_id(user_telegram_id)
+        if user_model is None:
+            user_id = self.repository.insert_user(user_telegram_id)
+            return user_id
+        else:
+            return user_model.id
+
+    def create_user_shopping_list(self, user_id):
+        user_shopping_list = self.repository.fetch_user_shop_lists(user_id)
+        if (user_shopping_list is None) or (len(user_shopping_list) == 0):
+            shop_list_id = self.repository.insert_shop_list(user_id)
+            return shop_list_id
+        else:
+            return user_shopping_list[0].id
 
     # this function calls when a query come
     def parameter_calculator(self, user_model):
@@ -37,7 +48,6 @@ class QueryProcessor:
             return response
         elif self.last_response == "چه مقدار؟":
             # todo how to add a product into database
-            
             response = "این کالا به سبد خرید شما اضافه شد."
             return response
         else:
