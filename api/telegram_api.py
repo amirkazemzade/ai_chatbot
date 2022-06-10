42a4e7a2e740664c5e09f7132418f4c14a835520
Amir Kazemzade <76568952+amirkzm0098@users.noreply.github.com>
2022-06-10 15:24:54 +0430

    feat: called parameter calculator in api

--- api/telegram_api.py
+++ api/telegram_api.py
@@ -1,4 +1,3 @@
-
 import constants
 import telebot
 from logic import QueryProcessor
@@ -7,23 +6,24 @@ ai_bot = telebot.TeleBot(constants.API_TOKEN)
 
 
 @ai_bot.message_handler(commands=['start'])
-def start(message):
+def start(message: telebot.types.Message):
     response = initialize_user(message)
     ai_bot.reply_to(message, response)
 
 
 @ai_bot.message_handler(func=lambda m: True)
-def all_messages(message):
-    resend_message(message)
+def all_messages(message: telebot.types.Message):
+    response_to_message(message)
 
 
-def initialize_user(message):
+def initialize_user(message: telebot.types.Message):
     processor = QueryProcessor()
     return processor.user_start(message.from_user)
 
 
-def resend_message(message):
-    ai_bot.reply_to(message, message.text)
+def response_to_message(message: telebot.types.Message):
+    processor = QueryProcessor()
+    return processor.parameter_calculator(message.from_user, message.text)
 
 
 ai_bot.polling()
